44e69ea4ce0b7006593c80e7c1c0eb6d5e8ff7ec
ianSama0217 <ian20000217@gmail.com>
2023-10-25 01:21:34 +0800

    complate ProjectView main page

---
+++ src/views/ProjectView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useProjectStore } from "../stores/project";
+import projectCard from "../components/projectComponents/projectCard.vue";
+
+const projectStore = useProjectStore();
+//解構資料
+const { projectObject } = storeToRefs(projectStore);
+</script>
+
+<template>
+  <div class="body">
+    <projectCard :cardData="projectObject" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.body {
+  padding: 0 10vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
