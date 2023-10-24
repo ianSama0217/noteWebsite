3de7bf3d9c6b35440962b42e6e270182d7cd9d1b
ianSama0217 <ian20000217@gmail.com>
2023-10-25 01:20:02 +0800

    complate NoteView's components

---
+++ src/components/noteComponents/noteCard.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  noteTitle: {
+    type: String,
+    default: "",
+  },
+  noteList: {
+    type: Array,
+    default: () => [],
+  },
+  noteLink: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <div class="noteCard">
+    <h3>{{ noteTitle }}</h3>
+    <RouterLink
+      v-for="(item, index) in noteList"
+      class="link"
+      :to="noteLink[index]"
+      >{{ item }}</RouterLink
+    >
+  </div>
+</template>
+
+<style scoped lang="scss">
+$themeColor: #f1dec9;
+$compontentColor: #a4907c;
+$BorderColor: #8d7b68;
+$iconColor: #c8b6a6;
+$textLightColor: #ffffff;
+$textDarkColor: #000000;
+.noteCard {
+  width: 20vw;
+  border: 3px solid $BorderColor;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 0.5rem;
+  margin: 1rem;
+  background-color: $themeColor;
+
+  h3 {
+    font-size: 1.5rem;
+    color: $textDarkColor;
+    align-self: center;
+    margin-bottom: 0.5rem;
+  }
+
+  .link {
+    text-decoration: none;
+    font-size: 1.2rem;
+    color: $textDarkColor;
+    padding: 0.5rem 0;
+    transition: 0.5s ease;
+
+    &:hover {
+      background-color: $BorderColor;
+      color: $textLightColor;
+    }
+  }
+}
+</style>
---
+++ src/components/noteComponents/noteCardGroup.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useNoteStore } from "../../stores/note.js";
+import NoteCard from "./noteCard.vue";
+
+const noteStore = useNoteStore();
+//解構noteStore內的資料
+const {
+  DirectivesTitle,
+  DirectivesList,
+  DirectivesLink,
+  RouterTitle,
+  RouterLink,
+  RouterList,
+  LifecycleTitle,
+  LifecycleLink,
+  LifecycleList,
+  CompositionAPITitle,
+  CompositionAPILink,
+  CompositionAPIList,
+  ComponentTitle,
+  ComponentLink,
+  ComponentList,
+  PiniaTitle,
+  PiniaLink,
+  PiniaList,
+} = storeToRefs(noteStore);
+</script>
+
+<template>
+  <div class="waterfall">
+    <!-- Directives -->
+    <NoteCard
+      :noteTitle="DirectivesTitle"
+      :noteList="DirectivesList"
+      :noteLink="DirectivesLink"
+      class="noteCard"
+    />
+    <!-- Router -->
+    <NoteCard
+      :noteTitle="RouterTitle"
+      :noteList="RouterList"
+      :noteLink="RouterLink"
+      class="noteCard"
+    />
+    <!-- Lifecycle -->
+    <NoteCard
+      :noteTitle="LifecycleTitle"
+      :noteList="LifecycleList"
+      :noteLink="LifecycleLink"
+      class="noteCard"
+    />
+    <!-- CompositionAPI -->
+    <NoteCard
+      :noteTitle="CompositionAPITitle"
+      :noteList="CompositionAPIList"
+      :noteLink="CompositionAPILink"
+      class="noteCard"
+    />
+    <!-- Component -->
+    <NoteCard
+      :noteTitle="ComponentTitle"
+      :noteList="ComponentList"
+      :noteLink="ComponentLink"
+      class="noteCard"
+    />
+    <!-- Pinia -->
+    <NoteCard
+      :noteTitle="PiniaTitle"
+      :noteList="PiniaList"
+      :noteLink="PiniaLink"
+      class="noteCard"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.waterfall {
+  column-count: 3;
+}
+.noteCard {
+  break-inside: avoid;
+}
+</style>
