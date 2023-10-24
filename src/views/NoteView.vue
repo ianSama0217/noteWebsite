740cfc6c8685dab8cf515159007e2472fec9c2b9
ianSama0217 <ian20000217@gmail.com>
2023-10-25 01:21:13 +0800

    complate NoteView main page

---
+++ src/views/NoteView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import noteCardGroup from "../components/noteComponents/noteCardGroup.vue";
+</script>
+
+<template>
+  <div class="body">
+    <noteCardGroup />
+  </div>
+</template>
+
+<style scoped lang="scss">
+$themeColor: #f1dec9;
+$compontentColor: #a4907c;
+$BorderColor: #8d7b68;
+$iconColor: #c8b6a6;
+$textLightColor: #ffffff;
+$textDarkColor: #000000;
+
+.body {
+  padding: 0 10vw;
+}
+</style>
