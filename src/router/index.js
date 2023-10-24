a8ca0e8131c71e748f36578be696c877a78c07e6
ianSama0217 <ian20000217@gmail.com>
2023-10-25 01:22:41 +0800

    add /note and /project router

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,16 @@ const router = createRouter({
       name: "home",
       component: () => import("../views/HomeView.vue"),
     },
+    {
+      path: "/note",
+      name: "note",
+      component: () => import("../views/NoteView.vue"),
+    },
+    {
+      path: "/project",
+      name: "project",
+      component: () => import("../views/ProjectView.vue"),
+    },
   ],
 });
 
