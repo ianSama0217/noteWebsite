b0d421853f6450d27a21ec6c12f0f04d70b8cf1f
ianSama0217 <ian20000217@gmail.com>
2023-10-25 17:05:26 +0800

    add noteView 5 children router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 //匯入project子路徑
 import singleProjectView from "../views/projectRouter/singleProjectView.vue";
+//匯入note子路徑
+import DirectivesNoteView from "../views/noteRouter/DirectivesNoteView.vue";
+import RouterNoteView from "../views/noteRouter/RouterNoteView.vue";
+import LifeCycleNoteView from "../views/noteRouter/LifeCycleNoteView.vue";
+import CompositionAPINoteView from "../views/noteRouter/CompositionAPINoteView.vue";
+import CompontentNoteView from "../views/noteRouter/CompontentNoteView.vue";
+import PiniaNoteView from "../views/noteRouter/PiniaNoteView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +21,32 @@ const router = createRouter({
       path: "/note",
       name: "note",
       component: () => import("../views/NoteView.vue"),
+      children: [
+        {
+          path: "/note/Directives/:domain",
+          component: DirectivesNoteView,
+        },
+        {
+          path: "/note/Router/:domain",
+          component: RouterNoteView,
+        },
+        {
+          path: "/note/LifeCycle/:domain",
+          component: LifeCycleNoteView,
+        },
+        {
+          path: "/note/CompositionAPI/:domain",
+          component: CompositionAPINoteView,
+        },
+        {
+          path: "/note/Compontent/:domain",
+          component: CompontentNoteView,
+        },
+        {
+          path: "/note/Pinia/:domain",
+          component: PiniaNoteView,
+        },
+      ],
     },
     {
       path: "/project",
