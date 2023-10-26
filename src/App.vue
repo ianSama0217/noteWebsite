bbd587ae547a94c025c94e4ae064ee77cd810eb5
ianSama0217 <ian20000217@gmail.com>
2023-10-26 14:10:31 +0800

    add background image

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,26 @@
 import { RouterView } from "vue-router";
 
 import navbar from "./components/navbar.vue";
+import foo from "./components/foo.vue";
 </script>
 
 <template>
   <div class="body">
     <navbar />
     <RouterView />
+    <foo />
   </div>
+  <img class="backgroundImg" src="../public/background.jpg" />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.backgroundImg {
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  top: 0%;
+  left: 0%;
+  filter: opacity(0.6);
+  z-index: -1;
+}
+</style>
